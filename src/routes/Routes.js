0453e0b15119559af40da1c848b53d6b430954b5
frimpongopoku <pongofrimi@gmail.com>
2021-09-12 10:44:46 +0400

    setup header right nicely :fire:

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -4,8 +4,8 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import React from "react";
 import Login from "../pages/auth/Login";
 import Register from "../pages/auth/Register";
-import { Text } from "react-native";
-
+import { Text, TouchableOpacity } from "react-native";
+import { Entypo, Ionicons, MaterialIcons } from "@expo/vector-icons";
 const Tabs = createBottomTabNavigator();
 const Stack = createStackNavigator();
 export const AuthStack = () => (
@@ -25,23 +25,85 @@ export const AuthStack = () => (
   </NavigationContainer>
 );
 const Dummy = ({ text, route }) => <Text>{route?.params?.title}</Text>;
+
+const getTabIcons = (params) => {
+  const {
+    tabName,
+    focused,
+    size = 24,
+    color = "black",
+    activeColor = "red",
+  } = params;
+  const TABS_AND_ICONS = {
+    News: (
+      <Entypo name="news" size={size} color={focused ? activeColor : color} />
+    ),
+    Shop: (
+      <Ionicons
+        name="cart-outline"
+        size={size}
+        color={focused ? activeColor : color}
+      />
+    ),
+    Rider: (
+      <MaterialIcons
+        name="delivery-dining"
+        size={size}
+        color={focused ? activeColor : color}
+      />
+    ),
+    Settings: (
+      <Ionicons
+        name="settings-outline"
+        size={size}
+        color={focused ? activeColor : color}
+      />
+    ),
+  };
+
+  return TABS_AND_ICONS[tabName];
+};
+
+const makeHeaderRight = () => {
+  return () => (
+    <TouchableOpacity style={{ marginRight: 20 }}>
+      <Ionicons name="cart-outline" size={24} color={"red"} />
+    </TouchableOpacity>
+  );
+};
+// ------------------------------------------ MAIN APPLICATION BOTTOM TAB STACK ------------------
 export const ApplicationStack = () => (
   <NavigationContainer>
-    <Tabs.Navigator>
+    <Tabs.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused }) =>
+          getTabIcons({ focused, tabName: route.name }),
+        tabBarActiveTintColor: "red",
+        tabBarInactiveTintColor: "black",
+      })}
+    >
       <Tabs.Screen
-        name="Live Trips"
+        name="News"
         component={Dummy}
-        options={{ title: "Live Trips" }}
+        options={{
+          title: "News",
+          headerRight: makeHeaderRight(),
+        }}
       />
       <Tabs.Screen
-        name="Orders"
+        name="Shop"
         component={Dummy}
-        options={{ title: "Orders" }}
+        options={{ title: "Shop", headerRight: makeHeaderRight() }}
+      />
+      <Tabs.Screen
+        name="Rider"
+        component={Dummy}
+        options={{ title: "Rider", headerRight: makeHeaderRight() }}
       />
       <Tabs.Screen
         name="Settings"
         component={Dummy}
-        options={{ title: "Settings" }}
+        options={{ title: "Settings", headerRight: makeHeaderRight() }}
       />
     </Tabs.Navigator>
   </NavigationContainer>
