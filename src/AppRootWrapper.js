ce34e1d5fb4dcdcd8618aedccbc2faf6c559dead
frimpongopoku <pongofrimi@gmail.com>
2021-09-25 22:26:08 +0400

    hitting local routes now works with ngrok

--- src/AppRootWrapper.js
+++ src/AppRootWrapper.js
@@ -3,13 +3,17 @@ import { ActivityIndicator, Text, View } from "react-native";
 import { Provider } from "react-redux";
 import App from "../App";
 import store from "./redux/store";
+import { TEST_URL } from "./shared/urls";
 
 export default class AppRootWrapper extends Component {
-  state = {
-    loading: false,
-    authUser: undefined,
-  };
-
+  componentDidMount() {
+    fetch(TEST_URL)
+      // fetch("https://jsonplaceholder.typicode.com/todos/1")
+      
+      .then((response) => response.json())
+      .then((data) => console.log("DUDE I GOT IT", data))
+      .catch((e) => console.log("This is the error boana", e.toString()));
+  }
   render() {
     return (
       <Provider store={store}>
---
+++ src/shared/urls.js
@@ -0,0 +1,8 @@
+// const HOST = "192.168.0.38:8000";
+// const HOST = "127.0.0.1:8000";
+const HOST = "https://dac4-102-115-225-145.ngrok.io";
+
+export const TEST_URL =
+  HOST + "/api/donkomi/account/user/get/hnxuThJP3EdCHmTYrK3qlr2yBl32/";
+
+export const REGISTER_USER = HOST + "/user/create/";
