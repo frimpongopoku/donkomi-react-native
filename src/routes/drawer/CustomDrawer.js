2b835a6128ad1b0184ae5084dd5e9f8556147f27
frimpongopoku <pongofrimi@gmail.com>
2021-10-16 23:56:39 +0400

    Custom drawer works and is designed nicely now :fire:

--- src/routes/drawer/CustomDrawer.js
+++ src/routes/drawer/CustomDrawer.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import { AntDesign, FontAwesome } from "@expo/vector-icons";
 import { MaterialIcons } from "@expo/vector-icons";
 import { Feather } from "@expo/vector-icons";
 import {
   DrawerContentScrollView,
-  DrawerItemList,
   DrawerItem,
 } from "@react-navigation/drawer";
 import { STYLES } from "../../shared/ui";
+import { Defaults } from "../../shared/classes/Defaults";
 const makeIcon = ({ iconName, type, color = "black" }) => {
   switch (type) {
     case "fa":
@@ -43,36 +43,65 @@ const menu = [
 ];
 export default function CustomDrawer(props) {
   const { navigation, state } = props;
-  console.log("I am teh navigation state", state.index, state.name);
   return (
     <DrawerContentScrollView>
       <View
         style={{
           width: "100%",
           height: 200,
-          backgroundColor: "whitesmoke",
+          backgroundColor: STYLES.theme.blue,
           marginBottom: 10,
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
         }}
-      ></View>
-      {menu.map((m, index) => {
-        const { iconType, iconName, url } = m;
-        const selected = state.index === index;
-        return (
-          <DrawerItem
-            key={index.toString()}
-            icon={() =>
-              makeIcon({ iconName, type: iconType, color: STYLES.theme.blue })
-            }
-            label={m.name}
-            onPress={() => navigation.navigate(url)}
-            activeBackgroundColor="red"
-            activeTintColor="blue"
-            labelStyle={{ fontSize: 15, color: STYLES.theme.blue }}
-            style={{ backgroundColor: selected ? "whitesmoke" : "white" }}
-          />
-        );
-      })}
-      {/* <DrawerItemList {...props} labelStyle={{ fontSize: 15 }} /> */}
+      >
+        <Image
+          source={Defaults.getBurgerImage()}
+          style={{
+            height: 85,
+            width: 85,
+            borderRadius: 155,
+            borderWidth: 3,
+            borderColor: "white",
+          }}
+        />
+        <Text
+          style={{
+            marginTop: 10,
+            fontWeight: "bold",
+            color: "white",
+            fontSize: 15,
+          }}
+        >
+          Frimpong Opoku Agyemang
+        </Text>
+      </View>
+      <View style={{ flex: 1, height: "100%" }}>
+        {menu.map((m, index) => {
+          const { iconType, iconName, url } = m;
+          const selected = state.index === index;
+          return (
+            <DrawerItem
+              key={index.toString()}
+              icon={() =>
+                makeIcon({ iconName, type: iconType, color: STYLES.theme.blue })
+              }
+              label={m.name}
+              onPress={() => navigation.navigate(url)}
+              labelStyle={{ fontSize: 15, color: STYLES.theme.blue }}
+              style={{ backgroundColor: selected ? "#edf3ff" : "white" }}
+            />
+          );
+        })}
+        <DrawerItem
+          label="Sign Out"
+          icon={() =>
+            makeIcon({ iconName: "logout", type: "ant", color: "red" })
+          }
+          labelStyle={{ fontSize: 15, color: "red" }}
+        />
+      </View>
     </DrawerContentScrollView>
   );
 }
--- src/shared/urls.js
+++ src/shared/urls.js
@@ -1,6 +1,6 @@
 // const HOST = "192.168.0.38:8000";
 // const HOST = "127.0.0.1:8000";
-const HOST = "http://f1d0-102-115-238-39.ngrok.io" + "/api/donkomi";
+const HOST = "http://ab61-102-115-238-39.ngrok.io" + "/api/donkomi";
 
 export const TEST_URL =
   HOST + "/account/user/get/hnxuThJP3EdCHmTYrK3qlr2yBl32/";
